60c2fa089301eec1a2dcbab2cf022abf01979709
Maxime <maxime.dacisac@hesias.fr>
2021-12-15 14:25:46 +0100

    groupe de serialisation

--- api/src/Entity/House.php
+++ api/src/Entity/House.php
@@ -10,17 +10,21 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use App\Repository\HouseRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 
 #[ORM\Entity(repositoryClass: HouseRepository::class)]
 #[ApiResource(
     collectionOperations: ['get', 'post'],
     itemOperations: ['get', 'put', 'delete'],
+    denormalizationContext: ['groups' => ['write']],
+    normalizationContext: ['groups' => ['read']],
 )]
 #[ApiFilter(SearchFilter::class, properties: ['name' => 'partial'])]
 #[ApiFilter(DateFilter::class, properties: ['createdDate'])]
 #[ApiFilter(BooleanFilter::class, properties: ['available'])]
 #[ApiFilter(OrderFilter::class, properties: ['number'])]
+
 class House
 {
     #[ORM\Id]
@@ -29,16 +33,20 @@ class House
     private $id;
 
     #[ORM\Column(type: 'integer')]
+    #[Groups(["read", "write"])]
     private $number;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(["read", "write"])]
     private $name;
 
     #[ORM\Column(type: 'datetime')]
     #[Gedmo\Timestampable(on: 'create')]
+    #[Groups("write")]
     private $createdDate;
 
     #[ORM\Column(type: 'boolean')]
+    #[Groups(["read", "write"])]
     private $available;
 
     public function getId(): ?int
